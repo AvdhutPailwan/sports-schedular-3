528914940faa3225d363bebdca42e6552fd3d93f
Avdhut Pailwan <avdhutpailwan@gmail.com>
2023-06-01 22:42:10 +0530

    Try 3. Initial commit. Database configuration and Session model

---
+++ migrations/20230601170535-create-session.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Sessions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      time: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      place: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      players: {
+        type: Sequelize.ARRAY(Sequelize.STRING),
+      },
+      noOfPlayers: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Sessions');
+  }
+};
