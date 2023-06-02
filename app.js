7d7a49db518612bef24d59294042b47453199e5e
Avdhut Pailwan <avdhutpailwan@gmail.com>
2023-06-02 23:10:44 +0530

    Prod script updated

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.redirect("/sportSession");
 });
 
 app.get("/sportSession", async (req, res) => {
